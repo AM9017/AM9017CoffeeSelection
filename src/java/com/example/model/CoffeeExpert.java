d11c15700e7a83c14789e1d7e1e1b8a51a994498
AM9017 <kapapage@inf.uth.gr>
2014-10-26 18:44:15 +0200

    Coffee Select code

---
+++ src/java/com/example/model/CoffeeExpert.java
@@ -0,0 +1,31 @@
+package com.example.model;
+
+import java.util.*;
+
+public class CoffeeExpert {
+   public List<String> getTypes(String type) {
+
+     List<String> types = new ArrayList<String>();
+
+     if (type.equals("milky")) {
+       types.add("latte");
+       types.add("cappuccino");
+     }
+     else if (type.equals("froffy")) {
+       types.add("latte");
+       types.add("cappuccino");
+       types.add("frappuccino");
+     }
+     else if (type.equals("icey")) {
+       types.add("frappuccino");
+     }
+     else if (type.equals("strong")) {
+       types.add("espresso");
+       types.add("double espresso");
+     }
+     else {
+       types.add("Vending Machine");
+     }
+     return(types);
+   }
+}
